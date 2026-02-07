cbdf38f32f050e3bffbffb4dfd47c92a14a56a91
grism13 <gmartinez.7707@unimar.edu.ve>
2026-02-06 23:03:32 -0400

    INTERFAZ MENU LISTA juajuajaua

--- Restaurant PALMERO proyecto 2/Form1.Designer.cs
+++ Restaurant PALMERO proyecto 2/Form1.Designer.cs
@@ -28,12 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            LOGO = new PictureBox();
+            LOGOTIPO = new PictureBox();
+            menubtn = new Button();
+            mesasbtn = new Button();
+            cajabtn = new Button();
+            ordenesbtn = new Button();
+            salirbtn = new Button();
+            OrdenesImg = new PictureBox();
+            SalirImg = new PictureBox();
+            MesaImg = new PictureBox();
+            MenuImg = new PictureBox();
+            CajaImg = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)LOGO).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)LOGOTIPO).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)OrdenesImg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)SalirImg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)MesaImg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)MenuImg).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)CajaImg).BeginInit();
+            SuspendLayout();
+            // 
+            // LOGO
+            // 
+            LOGO.BackColor = Color.Transparent;
+            LOGO.Image = Properties.Resources.logo_palmero_solo_texto_sin_fondo;
+            LOGO.Location = new Point(69, 22);
+            LOGO.Name = "LOGO";
+            LOGO.Size = new Size(885, 262);
+            LOGO.SizeMode = PictureBoxSizeMode.Zoom;
+            LOGO.TabIndex = 0;
+            LOGO.TabStop = false;
+            LOGO.Click += pictureBox1_Click;
+            // 
+            // LOGOTIPO
+            // 
+            LOGOTIPO.BackColor = Color.Transparent;
+            LOGOTIPO.Image = Properties.Resources.logo_website_icon_2;
+            LOGOTIPO.Location = new Point(460, 12);
+            LOGOTIPO.Name = "LOGOTIPO";
+            LOGOTIPO.Size = new Size(119, 95);
+            LOGOTIPO.SizeMode = PictureBoxSizeMode.Zoom;
+            LOGOTIPO.TabIndex = 1;
+            LOGOTIPO.TabStop = false;
+            LOGOTIPO.Click += pictureBox2_Click;
+            // 
+            // menubtn
+            // 
+            menubtn.BackColor = Color.MidnightBlue;
+            menubtn.BackgroundImage = Properties.Resources.logo_completo;
+            menubtn.Cursor = Cursors.Hand;
+            menubtn.FlatAppearance.BorderSize = 0;
+            menubtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            menubtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            menubtn.FlatStyle = FlatStyle.Flat;
+            menubtn.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            menubtn.ForeColor = Color.White;
+            menubtn.Location = new Point(259, 287);
+            menubtn.Name = "menubtn";
+            menubtn.Size = new Size(234, 46);
+            menubtn.TabIndex = 2;
+            menubtn.Text = "MENU";
+            menubtn.UseVisualStyleBackColor = false;
+            menubtn.Click += button1_Click;
+            // 
+            // mesasbtn
+            // 
+            mesasbtn.BackgroundImage = Properties.Resources.logo_completo;
+            mesasbtn.Cursor = Cursors.Hand;
+            mesasbtn.FlatAppearance.BorderSize = 0;
+            mesasbtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            mesasbtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            mesasbtn.FlatStyle = FlatStyle.Flat;
+            mesasbtn.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            mesasbtn.ForeColor = Color.White;
+            mesasbtn.Location = new Point(259, 366);
+            mesasbtn.Name = "mesasbtn";
+            mesasbtn.Size = new Size(234, 44);
+            mesasbtn.TabIndex = 3;
+            mesasbtn.Text = "MESAS";
+            mesasbtn.UseVisualStyleBackColor = true;
+            // 
+            // cajabtn
+            // 
+            cajabtn.BackgroundImage = Properties.Resources.logo_completo;
+            cajabtn.Cursor = Cursors.Hand;
+            cajabtn.FlatAppearance.BorderSize = 0;
+            cajabtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            cajabtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            cajabtn.FlatStyle = FlatStyle.Flat;
+            cajabtn.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cajabtn.ForeColor = Color.White;
+            cajabtn.Location = new Point(605, 287);
+            cajabtn.Name = "cajabtn";
+            cajabtn.Size = new Size(234, 46);
+            cajabtn.TabIndex = 7;
+            cajabtn.Text = "CAJA";
+            cajabtn.UseVisualStyleBackColor = true;
+            // 
+            // ordenesbtn
+            // 
+            ordenesbtn.BackgroundImage = Properties.Resources.logo_completo;
+            ordenesbtn.Cursor = Cursors.Hand;
+            ordenesbtn.FlatAppearance.BorderSize = 0;
+            ordenesbtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            ordenesbtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            ordenesbtn.FlatStyle = FlatStyle.Flat;
+            ordenesbtn.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ordenesbtn.ForeColor = Color.White;
+            ordenesbtn.Location = new Point(605, 366);
+            ordenesbtn.Name = "ordenesbtn";
+            ordenesbtn.Size = new Size(234, 44);
+            ordenesbtn.TabIndex = 6;
+            ordenesbtn.Text = "ORDENES";
+            ordenesbtn.UseVisualStyleBackColor = true;
+            // 
+            // salirbtn
+            // 
+            salirbtn.BackColor = Color.MidnightBlue;
+            salirbtn.BackgroundImage = Properties.Resources.logo_completo;
+            salirbtn.Cursor = Cursors.Hand;
+            salirbtn.FlatAppearance.BorderSize = 0;
+            salirbtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            salirbtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            salirbtn.FlatStyle = FlatStyle.Flat;
+            salirbtn.Font = new Font("Times New Roman", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            salirbtn.ForeColor = Color.White;
+            salirbtn.Location = new Point(42, 12);
+            salirbtn.Name = "salirbtn";
+            salirbtn.Size = new Size(65, 21);
+            salirbtn.TabIndex = 5;
+            salirbtn.Text = "SALIR";
+            salirbtn.UseVisualStyleBackColor = false;
+            salirbtn.Click += button6_Click;
+            // 
+            // OrdenesImg
+            // 
+            OrdenesImg.Image = Properties.Resources.Diseño_sin_título__1_;
+            OrdenesImg.Location = new Point(513, 339);
+            OrdenesImg.Name = "OrdenesImg";
+            OrdenesImg.Size = new Size(99, 111);
+            OrdenesImg.SizeMode = PictureBoxSizeMode.Zoom;
+            OrdenesImg.TabIndex = 10;
+            OrdenesImg.TabStop = false;
+            // 
+            // SalirImg
+            // 
+            SalirImg.Image = Properties.Resources.Diseño_sin_título__2_;
+            SalirImg.Location = new Point(-12, -10);
+            SalirImg.Name = "SalirImg";
+            SalirImg.Size = new Size(75, 62);
+            SalirImg.SizeMode = PictureBoxSizeMode.Zoom;
+            SalirImg.TabIndex = 11;
+            SalirImg.TabStop = false;
+            SalirImg.Click += pictureBox3_Click;
+            // 
+            // MesaImg
+            // 
+            MesaImg.Image = Properties.Resources.Diseño_sin_título__3_;
+            MesaImg.Location = new Point(174, 339);
+            MesaImg.Name = "MesaImg";
+            MesaImg.Size = new Size(102, 111);
+            MesaImg.SizeMode = PictureBoxSizeMode.Zoom;
+            MesaImg.TabIndex = 12;
+            MesaImg.TabStop = false;
+            // 
+            // MenuImg
+            // 
+            MenuImg.Image = Properties.Resources.Diseño_sin_título__4_;
+            MenuImg.Location = new Point(174, 249);
+            MenuImg.Name = "MenuImg";
+            MenuImg.Size = new Size(102, 111);
+            MenuImg.SizeMode = PictureBoxSizeMode.Zoom;
+            MenuImg.TabIndex = 13;
+            MenuImg.TabStop = false;
+            // 
+            // CajaImg
+            // 
+            CajaImg.Image = Properties.Resources.Diseño_sin_título;
+            CajaImg.Location = new Point(513, 249);
+            CajaImg.Name = "CajaImg";
+            CajaImg.Size = new Size(99, 111);
+            CajaImg.SizeMode = PictureBoxSizeMode.Zoom;
+            CajaImg.TabIndex = 14;
+            CajaImg.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            BackColor = Color.GhostWhite;
+            ClientSize = new Size(1033, 450);
+            Controls.Add(cajabtn);
+            Controls.Add(ordenesbtn);
+            Controls.Add(salirbtn);
+            Controls.Add(mesasbtn);
+            Controls.Add(menubtn);
+            Controls.Add(LOGOTIPO);
+            Controls.Add(SalirImg);
+            Controls.Add(MenuImg);
+            Controls.Add(MesaImg);
+            Controls.Add(CajaImg);
+            Controls.Add(LOGO);
+            Controls.Add(OrdenesImg);
+            ForeColor = SystemColors.ControlLightLight;
+            Name = "Form1";
+            Text = "EL PALMERO RESTAURANT";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)LOGO).EndInit();
+            ((System.ComponentModel.ISupportInitialize)LOGOTIPO).EndInit();
+            ((System.ComponentModel.ISupportInitialize)OrdenesImg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)SalirImg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MesaImg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MenuImg).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CajaImg).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox LOGO;
+        private PictureBox LOGOTIPO;
+        private Button menubtn;
+        private Button mesasbtn;
+        private Button cajabtn;
+        private Button ordenesbtn;
+        private Button salirbtn;
+        private PictureBox OrdenesImg;
+        private PictureBox SalirImg;
+        private PictureBox MesaImg;
+        private PictureBox MenuImg;
+        private PictureBox CajaImg;
     }
 }
--- Restaurant PALMERO proyecto 2/Form1.cs
+++ Restaurant PALMERO proyecto 2/Form1.cs
@@ -6,5 +6,40 @@ namespace Restaurant_PALMERO_proyecto_2
         {
             InitializeComponent();
         }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
