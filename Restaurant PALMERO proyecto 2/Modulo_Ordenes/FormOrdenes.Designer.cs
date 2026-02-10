4a88cc6f4fcd83a32feac9f025567c4d0a4ee52e
eliezer230 <erodrguez.6801@unimar.edu.ve>
2026-02-10 16:49:56 -0400

    hola

--- Restaurant PALMERO proyecto 2/Modulo_Ordenes/FormOrdenes.Designer.cs
+++ Restaurant PALMERO proyecto 2/Modulo_Ordenes/FormOrdenes.Designer.cs
@@ -28,124 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            cmbPlatos = new ComboBox();
             label1 = new Label();
-            btnAgregar = new Button();
-            numCantidad = new NumericUpDown();
             label2 = new Label();
-            dgvPedido = new DataGridView();
             label3 = new Label();
-            btnFinalizar = new Button();
-            ((System.ComponentModel.ISupportInitialize)numCantidad).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dgvPedido).BeginInit();
+            label4 = new Label();
+            label5 = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
             SuspendLayout();
-            // 
-            // cmbPlatos
-            // 
-            cmbPlatos.FormattingEnabled = true;
-            cmbPlatos.Location = new Point(77, 147);
-            cmbPlatos.Name = "cmbPlatos";
-            cmbPlatos.Size = new Size(178, 28);
-            cmbPlatos.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(92, 87);
+            label1.Location = new Point(51, 72);
             label1.Name = "label1";
-            label1.Size = new Size(136, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Platos Disponobles";
-            // 
-            // btnAgregar
-            // 
-            btnAgregar.Location = new Point(77, 320);
-            btnAgregar.Name = "btnAgregar";
-            btnAgregar.Size = new Size(178, 64);
-            btnAgregar.TabIndex = 3;
-            btnAgregar.Text = "Agregar plato";
-            btnAgregar.UseVisualStyleBackColor = true;
-            btnAgregar.Click += btnAgregar_Click_1;
-            // 
-            // numCantidad
-            // 
-            numCantidad.Location = new Point(78, 248);
-            numCantidad.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
-            numCantidad.Name = "numCantidad";
-            numCantidad.Size = new Size(177, 27);
-            numCantidad.TabIndex = 4;
-            numCantidad.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            label1.Size = new Size(56, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Mesa 1";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(131, 206);
+            label2.Location = new Point(210, 72);
             label2.Name = "label2";
-            label2.Size = new Size(63, 20);
-            label2.TabIndex = 5;
-            label2.Text = "Numero";
-            // 
-            // dgvPedido
-            // 
-            dgvPedido.BackgroundColor = Color.White;
-            dgvPedido.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvPedido.GridColor = Color.White;
-            dgvPedido.Location = new Point(364, 147);
-            dgvPedido.Name = "dgvPedido";
-            dgvPedido.RowHeadersWidth = 51;
-            dgvPedido.Size = new Size(361, 159);
-            dgvPedido.TabIndex = 6;
+            label2.Size = new Size(56, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Mesa 2";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(484, 87);
+            label3.Location = new Point(373, 72);
             label3.Name = "label3";
-            label3.Size = new Size(107, 20);
-            label3.TabIndex = 7;
-            label3.Text = "Platos pedidos";
+            label3.Size = new Size(56, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Mesa 3";
             // 
-            // btnFinalizar
+            // label4
             // 
-            btnFinalizar.Location = new Point(458, 345);
-            btnFinalizar.Name = "btnFinalizar";
-            btnFinalizar.Size = new Size(178, 64);
-            btnFinalizar.TabIndex = 8;
-            btnFinalizar.Text = "Realizar Pedido";
-            btnFinalizar.UseVisualStyleBackColor = true;
-            btnFinalizar.Click += btnFinalizar_Click_1;
+            label4.AutoSize = true;
+            label4.Location = new Point(536, 72);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Mesa 4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(695, 72);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Mesa 5";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(38, 161);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 56);
+            button2.TabIndex = 6;
+            button2.Text = "ESTADO DE PEDIDO";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(197, 161);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 56);
+            button1.TabIndex = 7;
+            button1.Text = "ESTADO DE PEDIDO";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(353, 161);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 56);
+            button3.TabIndex = 8;
+            button3.Text = "ESTADO DE PEDIDO";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(520, 161);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 56);
+            button4.TabIndex = 9;
+            button4.Text = "ESTADO DE PEDIDO";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(672, 161);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 56);
+            button5.TabIndex = 10;
+            button5.Text = "ESTADO DE PEDIDO";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(51, 260);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(68, 24);
+            checkBox1.TabIndex = 11;
+            checkBox1.Text = "LISTO";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(210, 260);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(68, 24);
+            checkBox2.TabIndex = 12;
+            checkBox2.Text = "LISTO";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(361, 260);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(68, 24);
+            checkBox3.TabIndex = 13;
+            checkBox3.Text = "LISTO";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(536, 260);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(68, 24);
+            checkBox4.TabIndex = 14;
+            checkBox4.Text = "LISTO";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(683, 260);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(68, 24);
+            checkBox5.TabIndex = 15;
+            checkBox5.Text = "LISTO";
+            checkBox5.UseVisualStyleBackColor = true;
             // 
             // FormOrdenes
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1092, 586);
-            Controls.Add(btnFinalizar);
+            ClientSize = new Size(1312, 645);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(label5);
+            Controls.Add(label4);
             Controls.Add(label3);
-            Controls.Add(dgvPedido);
             Controls.Add(label2);
-            Controls.Add(numCantidad);
-            Controls.Add(btnAgregar);
             Controls.Add(label1);
-            Controls.Add(cmbPlatos);
             Name = "FormOrdenes";
             Text = "FormOrdenes";
-            Load += FormOrdenes_Load;
-            ((System.ComponentModel.ISupportInitialize)numCantidad).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dgvPedido).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private ComboBox cmbPlatos;
         private Label label1;
-        private Button btnAgregar;
-        private NumericUpDown numCantidad;
         private Label label2;
-        private DataGridView dgvPedido;
         private Label label3;
-        private Button btnFinalizar;
+        private Label label4;
+        private Label label5;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
     }
 }
